85d08fed4aeb68c9ef6b10899435b53d4be4ba04
Dante Santelli <dante.a.santelli@gmail.com>
2017-03-27 15:54:44 -0300

    Video #6

---
+++ ListasSimples/ListasSimples/ListasSimples/Elemento.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasSimples
+{
+    public class Elemento
+    {
+        
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+    }
+
+    public class ListaElementos
+    {
+        public List<Elemento> elementosList;
+
+        public ListaElementos()
+        {
+            elementosList = new List<Elemento>();
+            LoadElements();
+        }
+
+        public void LoadElements()
+        {
+            elementosList.Add(new ListasSimples.Elemento
+            {
+                nombre = " Elemento 1",
+                descripcion = " Descripcion al azar"
+            });
+
+            elementosList.Add(new ListasSimples.Elemento
+            {
+                nombre = " asdfasdas dasdas",
+                descripcion = "  dasdas dsadsdad"
+            });
+        }
+    }
+}
---
+++ ListasSimples/ListasSimples/ListasSimples/Home.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ListasSimples
+{
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        //Cuando aparezca la página
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ListaElementos elementos = new ListaElementos();
+            lvElementos.ItemsSource = elementos.elementosList;
+            lvElementos.ItemSelected += LvElementos_ItemSelected;
+        }
+
+        private void LvElementos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if( e.SelectedItem != null){
+                var element = e.SelectedItem as Elemento;
+                DisplayAlert("Listas", element.descripcion , "Aceptar");
+            }
+
+        }
+    }
+}
